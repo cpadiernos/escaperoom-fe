f6e0e101f37a354bbeaaf8b8b43395dcc45eef4f
Carol <cpadiernos@gmail.com>
2020-08-18 14:47:35 -0400

    Add games table and getGames functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Games</h1>
+    <game-table :games="games"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameTable from './components/GameTable.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    GameTable,
+  },
+  data() {
+    return {
+      games: [],
+    }
+  },
+  mounted() {
+    this.getGames()
+  },
+  methods: {
+    async getGames() {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/games')
+        const data = await response.json()
+        this.games = data
+        data.forEach(game => {
+          for (var key of Object.keys(game)) {
+            let new_key = key.replace(/_[a-z]/g, function(regexMatch) {
+                    if (key.indexOf(regexMatch) == 0) {
+                      return regexMatch[1]
+                    } else {
+                      return regexMatch[1].toUpperCase()
+                    }
+                  });
+            if (new_key != key) {
+              game[new_key] = game[key];
+              delete game[key];
+            }
+          }
+        });
+      } catch (error) {
+        console.error(error)
+      }
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/GameTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="game-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Game Name</th>
+          <th>Description</th>
+          <th>Number of Players</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="game in games" :key="game.id">
+          <td>{{ game.name }}</td>
+          <td>{{ game.description }}</td>
+          <td>{{ game.numOfPlayers }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'game-table',
+    props: {
+      games: Array,
+    }
+  }
+</script>
+
+<style scoped></style>
