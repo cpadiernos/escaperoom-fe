d0a497047c42675416faa2c4a38bee693eaa8751
Carol <cpadiernos@gmail.com>
2020-08-19 18:02:14 -0400

    Add form validation

--- src/components/GameForm.vue
+++ src/components/GameForm.vue
@@ -2,11 +2,33 @@
   <div id="game-form">
     <form @submit.prevent="handleSubmit">
       <label>Game name</label>
-      <input v-model="game.name" type="text" />
+      <input
+        v-model="game.name"
+        type="text"
+        :class="{ 'has-error': submitting && invalidName }"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+        ref="first"
+      />
       <label>Game description</label>
-      <input v-model="game.description" type="text" />
+      <input
+        v-model="game.description"
+        type="text"
+        :class="{ 'has-error': submitting && invalidDescription }"
+        @focus="clearStatus"
+      />
       <label>Number of players</label>
-      <input v-model="game.numOfPlayers" type="number" />
+      <input
+        v-model="game.numOfPlayers"
+        type="number"
+        :class="{ 'has-error': submitting && invalidNumOfPlayers }"
+      />
+      <p v-if="error && submitting" class="error-message">
+        Please fill out all the required fields.
+      </p>
+      <p v-if="success" class="success-message">
+        Game was successfully added.
+      </p>
       <button>Add Game</button>
     </form>
   </div>
@@ -17,6 +39,9 @@
     name: 'game-form',
     data() {
       return {
+        submitting: false,
+        error: false,
+        success: false,
         game: {
           name: '',
           description: '',
@@ -26,8 +51,59 @@
     },
     methods: {
       handleSubmit() {
+        this.submitting = true
+        this.clearStatus()
+        
+        if (
+          this.invalidName ||
+          this.invalidDescription ||
+          this.invalidNumOfPlayers
+        ) { this.error = true; return}
+          
         this.$emit('add:game', this.game)
+        this.$refs.first.focus()
+        this.game = {
+          name: '',
+          description: '',
+          numOfPlayers: '',
+        }
+        this.error = false
+        this.success = true
+        this.submitting = false
       },
+      clearStatus() {
+        this.error = false
+        this.success = false
+      }
+    },
+    computed: {
+      invalidName() {
+        return this.game.name === ''
+      },
+      invalidDescription() {
+        return this.game.description === ''
+      },
+      invalidNumOfPlayers() {
+        return this.game.numOfPlayers <= 0
+      }
     },
   }
 </script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+  
+  [class*='-message'] {
+    font-weight: 500;
+  }
+  
+  .error-message {
+    color: red;
+  }
+  
+  .success-message {
+    color: green;
+  }
+</style>
