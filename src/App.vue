313dd4195e98cd7c4f92909865c7c2cb99c17af0
Carol <cpadiernos@gmail.com>
2020-08-19 15:23:13 -0400

    Create basic add game functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app" class="small-container">
     <h1>Games</h1>
+    <game-form @add:game="addGame" />
+    <br />
     <game-table :games="games"/>
   </div>
 </template>
 
 <script>
 import GameTable from './components/GameTable.vue'
+import GameForm from './components/GameForm.vue'
 
 export default {
   name: 'app',
   components: {
     GameTable,
+    GameForm,
   },
   data() {
     return {
@@ -22,30 +26,56 @@ export default {
     this.getGames()
   },
   methods: {
+    snakeToCamel(str) {
+        return str.replace(/_[a-z]/g, function(regexMatch) {
+          if (str.indexOf(regexMatch) == 0) {
+            return regexMatch[1];
+          } else {
+            return regexMatch[1].toUpperCase();
+          }
+        });
+    },
+    camelToSnake(str) {
+      return str.replace(/[A-Z]/g, function(regexMatch) {
+        return "_" + regexMatch.toLowerCase();
+      });
+    },
+    convertKeyCase(obj, caseConverter) {
+      for (var key of Object.keys(obj)) {
+        let new_key = caseConverter(key);
+        if (new_key != key) {
+          obj[new_key] = obj[key];
+          delete obj[key];
+        }
+      }
+    },
     async getGames() {
       try {
         const response = await fetch('http://127.0.0.1:5000/api/games')
         const data = await response.json()
-        this.games = data
         data.forEach(game => {
-          for (var key of Object.keys(game)) {
-            let new_key = key.replace(/_[a-z]/g, function(regexMatch) {
-                    if (key.indexOf(regexMatch) == 0) {
-                      return regexMatch[1]
-                    } else {
-                      return regexMatch[1].toUpperCase()
-                    }
-                  });
-            if (new_key != key) {
-              game[new_key] = game[key];
-              delete game[key];
-            }
-          }
+          this.convertKeyCase(game, this.snakeToCamel);
         });
+        this.games = data;
       } catch (error) {
         console.error(error)
       }
     },
+    async addGame(game) {
+      try {
+        this.convertKeyCase(game, this.camelToSnake);
+        const response = await fetch('http://127.0.0.1:5000/api/games' ,{
+          method: 'POST',
+          body: JSON.stringify(game),
+          headers: {'Content-type': 'application/json; charset=UTF=8'}
+          });
+        const data = await response.json();
+        this.convertKeyCase(data, this.snakeToCamel);
+        this.games = [...this.games, data];
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
 </script>
---
+++ src/components/GameForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="game-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Game name</label>
+      <input v-model="game.name" type="text" />
+      <label>Game description</label>
+      <input v-model="game.description" type="text" />
+      <label>Number of players</label>
+      <input v-model="game.numOfPlayers" type="number" />
+      <button>Add Game</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'game-form',
+    data() {
+      return {
+        game: {
+          name: '',
+          description: '',
+          numOfPlayers: '',
+        },
+      }
+    },
+    methods: {
+      handleSubmit() {
+        this.$emit('add:game', this.game)
+      },
+    },
+  }
+</script>
